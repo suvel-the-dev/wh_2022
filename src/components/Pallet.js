0cafa6b5de4fd26dfe11182e3d68842c979783e5
suvelExcelblaze <suvelrathneswar08@gmail.com>
2021-05-24 19:58:13 +0530

    Added new pallet texture and randamized the render

--- src/components/Pallet.js
+++ src/components/Pallet.js
@@ -1,6 +1,25 @@
 import * as THREE from 'three'
 import { useLoader } from '@react-three/fiber'
 import boxSignsTexture from '../asset/texture/box_texture_3.jpg';
+import boxSignsTexture1 from '../asset/texture/box_texture.jpg';
+import boxSignsTexture2 from '../asset/texture/box_texture_4.JPG';
+
+const palletTextureObj = {
+    1: boxSignsTexture,
+    2: boxSignsTexture1,
+    3: boxSignsTexture2
+};
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
+const getPalletTexture = () => {
+    const randomTexture = palletTextureObj[getRandomIntInclusive(1, 3)];
+    return randomTexture;
+};
 
 const Pallet = ({
     pos,
@@ -8,7 +27,7 @@ const Pallet = ({
     ...props
 }) => {
     const texture_1 =
-        useLoader(THREE.TextureLoader, boxSignsTexture)
+        useLoader(THREE.TextureLoader, getPalletTexture())
     return (
         <mesh
             {...props}
